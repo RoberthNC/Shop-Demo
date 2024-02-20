77f114c3ddb623124960bb325890b5be74ee66c3
Roberto Núñez <robertonc1599@gmail.com>
2024-02-20 12:31:29 -0500

    GlobalPrefix agregado

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger('Bootstrap');
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
